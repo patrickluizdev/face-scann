1c4028c5c6185731f99dd80761a40aa4270a3b2b
Jatabara <patrickluizjf@hotmail.com>
2022-05-26 21:50:42 -0300

    finalização do projeto

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ function init(){
     const tracker = new tracking.ObjectTracker('face')
     tracking.track('#video', tracker, {camera: true})
     tracker.on('track', event => {
-        console.log(event)
-        //context.clearRect
+       // console.log(event)
+        context.clearRect(0,0,canvas.width, canvas.height)
+        event.data.forEach( rect => {
+            console.log(rect)
+            context.strokeStyle = '#FF0000'
+            context.lineWidth = 2 
+            context.strokeRect(rect.x, rect.y, rect.width, rect.height)
+            context.fillText(`x: ${rect.x}, w: ${rect.width}`, rect.x+rect.width + 20, rect.y + 20)
+            context.fillText(`y: ${rect.y}, h: ${rect.height}`, rect.x+rect.width + 20, rect.y + 40)
+        })
     })
     
 }
